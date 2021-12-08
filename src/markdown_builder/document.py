b476926a4529671dd04138cfceb5e84795858c4f
Romilly Cocking <romilly.cocking@gmail.com>
2021-12-08 17:40:33 +0000

    Separated out contents() and close()

--- src/markdown_builder/document.py
+++ src/markdown_builder/document.py
@@ -1,22 +1,45 @@
+"""Builds a Markdown Document"""
+
 from io import StringIO
 
 
 def bullet(text):
+    """ turns raw text into a markdown bullet"""
     return '- ' + text
 
 
-def make_link(text, link):
+def make_link(text: str, link: str) -> str:
+
+    """
+    Creates a markdown link
+
+    :param text: the text to display
+    :param link: the link target
+    :return: the formatted link
+    """
     return '[%s](%s)' % (text, link)
+
+# TODO: Add code to support inline bold, italic etc.
 
 
 class MarkdownDocument:
     def __init__(self, indentation=None):
+
+        #: A container for the text of the markdown document
         self._contents = StringIO()
+
+        #: a string prefix used to indent text
         self.indentation = indentation if indentation else '    '
 
-    def append_text(self, text: str):
+    def append_text(self, text: str) -> 'MarkdownDocument':
+        """
+
+        :param text:
+        :return:
+        """
         self._contents.write(text)
         self._contents.write('\n')
+        return self
 
     def append_text_indented(self, text: str, depth: int):
         text = (depth*self.indentation)+text
@@ -31,9 +54,11 @@ class MarkdownDocument:
     def append_bullet(self, text: str, depth=0):
         self.append_text_indented(bullet(text), depth)
 
+    def close(self):
+        self._contents.close()
+
     def contents(self):
         result = self._contents.getvalue()
-        self._contents.close()
         return result
 
     def append_heading(self, text, level=1):
